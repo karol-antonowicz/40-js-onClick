7a9e004e7d5b3236aba94a7b3fc45ac05d1c1385
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-06 00:15:40 +0200

    added test.js with some onClick events

---
+++ test.js
@@ -0,0 +1,14 @@
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function (){
+    if (content.className == "open") {
+    // shrink //
+    content.className = "";
+} else {
+    // expand //
+    content.className = "open"; 
+    button.innerHTML = "SHOW LESS"
+
+}
+}
